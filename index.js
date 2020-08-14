30ff3887de4e45e14a6039ce00de99a2cf3a5ceb
Yizhi Zhang <36008792+klwaamzhang@users.noreply.github.com>
2020-08-13 20:09:54 -0400

    navbar transition

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function closeNav() {
   $("#navbarSupportedContent").collapse("hide");
 }
+
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  if (scroll < 300) {
+    $(".fixed-top").css("transition", "1000ms ease");
+    $(".fixed-top").css("background", "rgba(247, 246, 244, 0.3)");
+  } else {
+    $(".fixed-top").css("background", "rgba(247, 246, 244)");
+  }
+});

d1313081aa61c1ad53d5a4d20fd2d99bf1de6fd5
Yizhi Zhang <36008792+klwaamzhang@users.noreply.github.com>
2020-08-14 15:46:20 -0400

    restore

--- index.js
+++ index.js
@@ -11,17 +11,3 @@ $(window).scroll(function () {
     $(".fixed-top").css("background", "rgba(247, 246, 244)");
   }
 });
-
-$('a[href^="#"]').on("click", function (event) {
-  var target = $(this.getAttribute("href"));
-
-  if (target.length) {
-    event.preventDefault();
-    $("html, body").stop().animate(
-      {
-        scrollTop: target.offset().top,
-      },
-      750
-    );
-  }
-});
